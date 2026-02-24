62ac0c6686d742831890e490ddecebd72b3e49f4
Dhruv Tangri <dhruv1998tangri@gmail.com>
2026-02-24 15:39:33 -0500

    added vercel link to CORS

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   # Vite default
         "http://localhost:3000",   # CRA default
-        "https://digital-twin-wine.vercel.app/",
+        "https://digital-twin-c3zvrdzc8-dhruv-s-projects-3d0c16b8.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
